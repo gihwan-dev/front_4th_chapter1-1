398c6f8afc75e67706ca5dad71653592889066c7
gihwan-dev <choegihwan@choegihwan-ui-MacBookPro.local>
2024-12-20 14:54:07 +0900

    style(eventProcessor): 가독성 향상을 위한 개행 추가

--- src/services/eventProcessor.ts
+++ src/services/eventProcessor.ts
@@ -19,12 +19,17 @@ export function removeEventListener(id: Id) {
 export function delegateEvent() {
   delegatableEvents.forEach((eventName) => {
     const $root = document.getElementById("root");
+
     $root.addEventListener(eventName, (event) => {
       if (!event.target) return;
+
       const eventMap = listenerMap.get((event.target as HTMLElement).id);
       const handler = eventMap?.[eventName];
+
       if (!eventMap || !handler) return;
+
       handler(event);
+
       event.stopPropagation();
     });
   });
